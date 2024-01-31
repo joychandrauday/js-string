780e69662915bb65401ce265a61bef5e001b6091
joychandrauday <joychandraud@gmail.com>
2024-02-01 04:26:31 +0600

    added some js

--- property.js
+++ property.js
@@ -1,3 +1,61 @@
-console.log('============fetching data from array')
+console.log('============fetching data from array============')
 var person1='ggg';
 
+console.log('=====task-1=====')
+const colors = {
+    red: "#ff0000",
+    green: "#00ff00",
+    blue: "#0000ff",
+    "golden rod": '#daa520'
+};
+console.log(colors["golden rod"]);
+
+console.log('=====task-2=====')
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+car.capacity="5";
+console.log(car);
+
+console.log('=====task-3=====')
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+console.log(student.physics.marks);
+
+console.log('=====task-4=====')
+let student2 = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+let numberProperties=Object.keys(student2).length;
+console.log(numberProperties)
+
+console.log('=====task-5=====')
+let typeOfKey='';
+let person2={
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true 
+}
+let personLength = Object.keys(person2).length;
+let values=Object.values(person2)
+for (let i = 0; i < personLength; i++) {
+    typeOfKey=typeof(values[i]);
+
+    for (const key in person2) {
+    
+        console.log('key:',`${key} | type:${typeOfKey}`);
+    }
+}
--- string.js
+++ string.js
@@ -7,3 +7,18 @@ const numbers=[33,55,64,73,35]
 
 console.log(capital.length)
 console.log(capital[3])
+
+//REVERSE STRING
+console.log('==========REVERSE A STRING==========')
+
+const sentence='i will be there for you.'
+let reversed='';
+for(const letter of sentence){
+    reversed= letter+ reversed;
+}
+console.log(reversed);
+
+for (let i = 0; i < sentence.length; i++) {
+    console.log(sentence[i]);
+    
+}
